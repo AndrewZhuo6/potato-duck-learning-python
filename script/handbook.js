7ae4542f5fcba7088e388fb7e695b7b339478729
Timothy Pardin <1820252100@bit.edu.cn>
2026-09-17 22:25:37 +0800

    web optimization and bug fix in playing video on story prescence

--- script/handbook.js
+++ script/handbook.js
@@ -359,7 +359,7 @@ function setActive(id) {
   if (topBtn) {
     window.addEventListener("scroll", () => {
       topBtn.classList.toggle("is-visible", window.scrollY > 600);
-    });
+    }, { passive: true });
     topBtn.addEventListener("click", () => {
       window.scrollTo({ top: 0, behavior: "smooth" });
     });
