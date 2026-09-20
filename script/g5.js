7979822e998b45d430b78b8894015407108e40da
Cornelius J. L. - 林靖光 <corneliusjlim@gmail.com>
2026-09-20 16:24:44 +0800

    Changed the look of the story background

--- script/g5.js
+++ script/g5.js
@@ -6,7 +6,7 @@ function addCustomCSS(cssCode) {
 
 const myCSS = `
     body {
-        background: var(--main-bg) url('../assets/images/g-preview/g5_cover.jpeg') center/cover fixed;
+        background: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('../assets/images/g-preview/g5_cover.jpeg') center/cover fixed;
     }
 `;
 
